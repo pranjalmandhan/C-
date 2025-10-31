9a40cb82fda150e9db80975292c9b0c547b66a45
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-10-31 23:22:56 +0500

    Display Month Name from Month Number

---
+++ programs/task 9.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cin>>x;
+	if(x==1)
+	{
+		cout<<"jan";
+	}
+	else if(x==2)
+	{
+		cout<<"feb";
+	}
+	else if(x==3)
+	{
+		cout<<"Mar";
+	}
+	else if(x==4)
+	{
+		cout<<"Apr";
+	}	
+	else if(x==5)
+	{
+		cout<<"may";
+	}
+	else if(x==6)
+	{
+		cout<<"jun";
+	}
+	else if(x==7)
+	{
+		cout<<"july";
+	}
+	else if(x==8)
+	{
+		cout<<"Aug";
+	}
+	else if(x==9)
+	{
+		cout<<"sep";
+	}
+	else if(x==10)
+	{
+		cout<<"oct";
+	}
+	else if(x==11)
+	{
+		cout<<"nov";
+	}
+	else if(x==12)
+	{
+		cout<<"dec";
+	}
+	else
+	{
+		cout<<"your input is invalid";
+	}
+}
