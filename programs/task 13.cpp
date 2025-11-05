027ac1eca5e10106970c71ee07b3e254ed758a1c
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-11-05 15:36:52 +0500

    Check Whether a Character is a Vowel or Consonant

---
+++ programs/task 13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char x;
+	cin>>x;
+	if(x=='a'||x=='e'||x=='i'||x=='o'||x=='o'||x=='u')
+	{
+		cout<<"Vowel";
+	}
+	else
+	{
+		cout<<"constant";
+	}
+}
