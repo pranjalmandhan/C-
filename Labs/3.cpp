72f02098fd4e218623d5d2e023148589becf16c2
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-11-06 11:43:42 +0500

    Header Pattern Printing

---
+++ Labs/3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"************************************************"<<endl;
+	cout<<"*	lab1					  **programming for scientist"<<endl;
+	cout<<"			**			CMS ID BS-CS1						*"<<endl;
+	cout<<"*	SUKKUR IBAU					*"<<endl;
+	cout<<"************************************************"<<endl;
+}
