2055a6bbf026f581ebd665a215e6efd25087598d
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-11-06 11:36:32 +0500

    Car Travel Distance Calculator

---
+++ programs/task 17.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"A car travel 70 miles per hour :";
+	cout<<"The distance the car will travel in 6 hours : "<<70*6<<endl;
+	cout<<"The distance the car will travel in 15 hours : "<<70*15<<endl;
+	cout<<"The distance the car will travel in 10 hours : "<<70*10<<endl;
+}
