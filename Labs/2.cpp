90343792e5c3d7d367c8dde8d9b7516be16d0918
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-11-06 11:42:35 +0500

    Character Pattern Printing

---
+++ Labs/2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"CCCCCCCCCCCCCCCCC				++						++"<<endl;
+	cout<<"CC								++						++"<<endl;
+	cout<<"CC						++++++++++++++++++		++++++++++++++++++"<<endl;
+	cout<<"CC						++++++++++++++++++		++++++++++++++++++"<<endl;
+	cout<<"CC								++						++"<<endl;
+	cout<<"CCCCCCCCCCCCCCCCC				++						++"<<endl;
+	
+}
