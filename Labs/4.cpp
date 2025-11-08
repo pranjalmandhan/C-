032d84c65dac1d92d5f4139b02f24f0224539e8c
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-11-08 21:38:49 +0500

    BIO-DATA Display Program

---
+++ Labs/4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\t\t\t\tBIO-DATA"<<endl;
+	cout<<"\t\t  __________________________________"<<endl;
+	cout<<"\t\t\tname:\t\tsiraj"<<endl;
+	cout<<"\t\t\tfather name:\tNazeer ahmed"<<endl;
+	cout<<"\t\t\tdob:\t\t15/1/2003"<<endl;
+	cout<<"\t\t\theight:\t\t5feet"<<endl;
+	cout<<"\t\t\tqualification:\tI.A"<<endl;
+}
