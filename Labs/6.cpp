d4bd2e9bc3d2e842a9de5058e07f26f4857da7b7
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2025-11-08 21:40:28 +0500

    Arithmetic Operations

---
+++ Labs/6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cin>>x>>y;
+	cout<<"Addition"<<endl;
+	cout<<x+y<<endl;
+	cout<<"Sub"<<endl;
+	cout<<x-y<<endl;
+	cout<<"multiplication"<<endl;
+	cout<<x*y<<endl;
+	cout<<"division"<<endl;
+	cout<<x/y;
+}
